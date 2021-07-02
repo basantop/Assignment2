619235b77c63d994789f14aa86d176c18c5d87a0
basantop <86840162+basantop@users.noreply.github.com>
2021-07-02 19:48:07 +0530

    day 4 asssignment submitted

---
+++ day4.py
@@ -0,0 +1,13 @@
+given_range = int(input("enter the ft"))
+
+if given_range < 1000:
+
+ print("Dear Pilot, you can land your plane safely")
+
+elif given_range > 1000 and given_range < 5000:
+
+ print("Dear Pilot, you are advised to bring down the pilot to 1000ft")
+
+else:
+
+ print("Dear Pilot, you can go around and try later")
